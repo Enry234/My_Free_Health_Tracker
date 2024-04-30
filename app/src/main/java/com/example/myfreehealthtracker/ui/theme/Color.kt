c8f7174aa113f3afc7008091f721ffe52f3ccbf8
Enrico Trettenero <enry@MacBook-Pro-di-Enrico.local>
2024-04-30 17:54:51 +0200

    Add Bottom Navigation Bar UI

--- app/src/main/java/com/example/myfreehealthtracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/myfreehealthtracker/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFF2E2E2E)
+val Primary = Color(0xFFFFA726)
+val Secondary = Color(0xFFEF6C00)
