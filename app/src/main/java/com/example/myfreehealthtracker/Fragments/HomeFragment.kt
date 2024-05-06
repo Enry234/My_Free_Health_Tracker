ae8834b99568c3fc5e5ffbf93b34ab5d53396a39
FrancescoPizzolato <francesco.pizzolato.3@studenti.unipd.it>
2024-05-06 16:08:45 +0200

    Drawer Navigation Fixed

--- app/src/main/java/com/example/myfreehealthtracker/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myfreehealthtracker/Fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myfreehealthtracker.Fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,6 +12,8 @@ import android.widget.ImageButton
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myfreehealthtracker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class HomeFragment : Fragment() {
 
@@ -21,29 +24,38 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val drawerLayout: DrawerLayout? = activity?.findViewById(R.id.home_drawer)
+        val drawerLayout = view.findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = view.findViewById<NavigationView>(R.id.navigation_view)
 
-        // Verifica se il DrawerLayout è nullo
-        if (drawerLayout == null) {
-            Log.e("Fragment", "DrawerLayout is null")
-            return view // Ritorna il layout del fragment senza impostare il click listener sul pulsante
+        val button: ImageButton = view.findViewById(R.id.btn_open_drawer)
+
+// Aprire il NavigationView
+        button.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
 
-        // Trova il pulsante nell'layout del fragment
-        val btnOpenDrawer: ImageButton = view.findViewById(R.id.btn_open_drawer)
-        btnOpenDrawer.setOnClickListener {
-            // Verifica se il Navigation Drawer è aperto o chiuso
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                // Se è aperto, chiudilo
-                drawerLayout.closeDrawer(GravityCompat.START)
-            } else {
-                // Se è chiuso, aprilo
-                drawerLayout.openDrawer(GravityCompat.START)
+// Chiudere il NavigationView
+        navView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            // Chiudi il NavigationView
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+
+        val bottomNav = view.findViewById<NavigationView>(R.id.navigation_view)
+
+
+        bottomNav.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.Logout -> makeCurrentFragment(HomeFragment())
             }
+            true
         }
 
 
         // Inflate the layout for this fragment
         return view
     }
+
 }
