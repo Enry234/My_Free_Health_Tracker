f1783385c8b5060c54a8be5d3ff8e17e9b8d7d06
lcalllegari1 <leonardo.callegari.1@studenti.unipd.it>
2024-05-13 22:16:32 +0200

    Add new meal fragment + link with health fragment

--- app/src/main/java/com/example/myfreehealthtracker/Fragments/HealthFragment.kt
+++ app/src/main/java/com/example/myfreehealthtracker/Fragments/HealthFragment.kt
@@ -2,8 +2,10 @@ package com.example.myfreehealthtracker.Fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.myfreehealthtracker.ExpandableFloatingActionButton
 import com.example.myfreehealthtracker.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -32,12 +34,17 @@ class HealthFragment : Fragment(R.layout.fragment_health) {
         )
 
         expandableFloatingActionButton.setChildActionListener(firstChild) {
-            Toast.makeText(view.context, "ACTION1", Toast.LENGTH_SHORT).show()
+            // NEW MEAL
+            view.findNavController().navigate(R.id.action_health_fragment_new_meal)
+            //Toast.makeText(view.context, "ACTION1", Toast.LENGTH_SHORT).show()
         }
         expandableFloatingActionButton.setChildActionListener(secondChild) {
             Toast.makeText(view.context, "ACTION2", Toast.LENGTH_SHORT).show()
         }
 
-
+        val content = arguments?.getString("content")
+        view.findViewById<TextView>(R.id.fragment_health_tv).apply {
+            if (content != "") text = content
+        }
     }
 }
