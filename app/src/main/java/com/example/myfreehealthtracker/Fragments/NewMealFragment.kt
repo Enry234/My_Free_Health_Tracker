f46be722e9fc0acc2278fab336ac5d749c2931d9
lcalllegari1 <leonardo.callegari.1@studenti.unipd.it>
2024-05-21 15:59:25 +0200

    Add add new existing food dialog

--- app/src/main/java/com/example/myfreehealthtracker/Fragments/NewMealFragment.kt
+++ app/src/main/java/com/example/myfreehealthtracker/Fragments/NewMealFragment.kt
@@ -2,8 +2,11 @@ package com.example.myfreehealthtracker.Fragments
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -11,6 +14,7 @@ import com.example.myfreehealthtracker.R
 
 class NewMealFragment : Fragment(R.layout.fragment_new_meal) {
 
+    private var foodList = mutableListOf<String>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -33,6 +37,33 @@ class NewMealFragment : Fragment(R.layout.fragment_new_meal) {
 
         dialogAddFood.findViewById<Button>(R.id.dialog_food_cancel_button).setOnClickListener {
             dialogAddFood.dismiss()
+        }
+
+        dialogAddFood.findViewById<Button>(R.id.dialog_food_existing_button).setOnClickListener {
+            val dialogAddExistingFood = Dialog(view.context)
+            dialogAddExistingFood.setContentView(R.layout.dialog_new_food_spinner)
+            val spinner = dialogAddExistingFood.findViewById<Spinner>(R.id.dialog_existing_spinner)
+
+            val existingFoodItems = listOf("pizzooooooooooooooooooooooooo", "trette", "calle", "carletto", "pizzo", "trette", "calle", "carletto", "pizzo", "trette", "calle", "carletto", "pizzo", "trette", "calle", "carletto")
+            val foodAdapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, existingFoodItems)
+            foodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = foodAdapter
+
+            dialogAddExistingFood.setOnCancelListener {
+                dialogAddFood.show()
+            }
+            dialogAddFood.dismiss()
+            dialogAddExistingFood.show()
+
+            dialogAddExistingFood.findViewById<Button>(R.id.dialog_existing_spinner_cancel).setOnClickListener {
+                dialogAddExistingFood.dismiss()
+                dialogAddFood.show()
+            }
+
+            dialogAddExistingFood.findViewById<Button>(R.id.dialog_existing_spinner_confirm).setOnClickListener {
+                foodList.add(spinner.selectedItem.toString())
+                dialogAddExistingFood.dismiss()
+            }
         }
     }
 }
