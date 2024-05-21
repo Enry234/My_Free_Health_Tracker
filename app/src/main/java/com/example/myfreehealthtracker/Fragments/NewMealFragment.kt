3be77873c74a2b0c1124ac5434d79c79a4e6265b
lcalllegari1 <leonardo.callegari.1@studenti.unipd.it>
2024-05-21 14:53:33 +0200

    Update dialog actions

--- app/src/main/java/com/example/myfreehealthtracker/Fragments/NewMealFragment.kt
+++ app/src/main/java/com/example/myfreehealthtracker/Fragments/NewMealFragment.kt
@@ -1,15 +1,16 @@
 package com.example.myfreehealthtracker.Fragments
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.myfreehealthtracker.R
 
 class NewMealFragment : Fragment(R.layout.fragment_new_meal) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -22,9 +23,16 @@ class NewMealFragment : Fragment(R.layout.fragment_new_meal) {
             )
         }
 
+        val dialogAddFood = Dialog(view.context)
+        dialogAddFood.setContentView(R.layout.fragment_new_meal_add_food_dialog)
+
         val addNewFoodBtn: Button = view.findViewById(R.id.fragment_new_meal_add_food)
         addNewFoodBtn.setOnClickListener {
+            dialogAddFood.show()
+        }
 
+        dialogAddFood.findViewById<Button>(R.id.dialog_food_cancel_button).setOnClickListener {
+            dialogAddFood.dismiss()
         }
     }
 }
