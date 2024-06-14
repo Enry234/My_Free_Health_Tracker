1fca219aae1eae5cf24497e9ad965751e4f50b16
lcalllegari1 <leonardo.callegari.1@studenti.unipd.it>
2024-06-14 18:48:39 +0200

    Fix theme in login page layout

--- app/src/main/java/com/example/myfreehealthtracker/viewmodel/login/LoginActivity.kt
+++ app/src/main/java/com/example/myfreehealthtracker/viewmodel/login/LoginActivity.kt
@@ -3,13 +3,16 @@ package com.example.myfreehealthtracker.viewmodel.login
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfreehealthtracker.ApplicationTheme
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val a = AuthPage()
         setContent {
-            a.LoginForm()
+            ApplicationTheme {
+                a.LoginForm()
+            }
         }
     }
 }
